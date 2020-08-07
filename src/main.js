e2e76a3c3f3503fca63f6c6c5f538eaf62c959b1
it_dog <it_dog_zhang@163.com>
2020-08-07 23:04:53 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './styles/test.css'
+let yy= {}
+Object.assign(yy,{xx:"xx"})
+const xx= ()=> {
+ alert(yy)
+}
+xx()
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,58 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const TerserJSPlugin = require('terser-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
 module.exports = {
+    mode: "development",
     entry: './src/main.js',
+    optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             title: "Webpack App",
             template: "public/index.html"
-        })
+        }),
+        new MiniCssExtractPlugin()
     ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            insert: 'head',
+                            esModule: true
+                        }
+                    },
+                    {
+                        loader: 'css-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-transform-runtime']
+                    }
+                }
+            }
+        ]
+    },
     devServer: {
-      contentBase: './dist',
+        contentBase: './dist',
+        port: 8080,
+        progress:true
     },
     output: {
         filename: '[name].js',
